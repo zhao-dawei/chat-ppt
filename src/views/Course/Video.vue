599f0b5309d627102c30e4920b640479b8d497a8
zhangzhi1979815592 <1979815592@qq.com>
2023-12-14 14:55:38 +0800

    视频页面

--- src/views/Course/Video.vue
+++ src/views/Course/Video.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="course_video_wrapper">
-        video
-    </div>
+  <div class="course_video_wrapper">
+    课程视频已完成制作，单击下方链接确认下载。
+  </div>
+  <button class="submit_btn"  @click="next">下载课程视频</button>
+  <button class="next_btn"  @click="next">进入下一页</button>
 </template>
 
 <script setup>
@@ -9,5 +11,27 @@
 </script>
 
 <style>
+.submit_btn {
+  width: 220px;
+  line-height: 40px;
+  color: #fff;
+  background: tomato;
+  outline: none;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  margin-top: 20px;
+}
+.next_btn {
+  width: 220px;
+  line-height: 40px;
+  color: #fff;
+  background: tomato;
+  outline: none;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  margin-top: 207px;
+}
 
 </style>
